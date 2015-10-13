b2782b2c2721bf5c9d22db02eee2191cfe407467
Nickkun <nickkun@ancle.kr>
2015-10-13 20:50:17 +0900

    router 모듈 추가

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 
 
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route) {
 	function onRequest(req, res) {
+		var pathname = url.parse(req.url).pathname;
+		console.log("Request for " + pathname + " received.");
+
+		route(pathname);
+
 		res.writeHead(200, {"Content=Type" : "text/plain"});
 		res.write("Hello World!");
 		res.end();
 	}
-	
+
 	http.createServer(onRequest).listen(8888);
 	console.log("Server Started on 8888 ports");
 }
