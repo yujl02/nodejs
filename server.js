a3e348e8f6a8844814e084518eebca77658d3389
Nickkun <nickkun@ancle.kr>
2015-10-13 20:31:33 +0900

    index.js 추가, server.js 모듈화

--- server.js
+++ server.js
@@ -2,10 +2,15 @@
 
 var http = require("http");
 
-function onRequest(req, res) {
-	res.writeHead(200, {"Content=Type" : "text/plain"});
-	res.write("Hello World!");
-	res.end();
-}).listen(8888);
+function start() {
+	function onRequest(req, res) {
+		res.writeHead(200, {"Content=Type" : "text/plain"});
+		res.write("Hello World!");
+		res.end();
+	}
+	
+	http.createServer(onRequest).listen(8888);
+	console.log("Server Started on 8888 ports");
+}
 
-console.log("Server Started on 8888 ports");
+exports.start = start;
